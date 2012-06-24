5ca8dd75c6c9d5e2f2081e4be165e529c65c7206
Khajiit333 <marcin.marzynski@gmail.com>
2012-06-24 12:35:34 +0200

    YAML File added

--- WSDLResolver/src/pl/edu/agh/yamlconverter/YAMLConverter.java
+++ WSDLResolver/src/pl/edu/agh/yamlconverter/YAMLConverter.java
@@ -1,12 +1,25 @@
 package pl.edu.agh.yamlconverter;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.LinkedHashMap;
+
+import org.yaml.snakeyaml.Yaml;
+
 public class YAMLConverter {
 
 	/**
 	 * @param args
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		InputStream io = new FileInputStream("UserService.yml");
+		Yaml yaml = new Yaml();
+		Object obj = yaml.load(io);
+		LinkedHashMap<String, Object> map = (LinkedHashMap<String, Object>) obj;
+		System.out.println(map);
 
 	}
 
