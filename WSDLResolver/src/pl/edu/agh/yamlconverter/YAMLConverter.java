1235e88ab6e16c253210ec827e357cabb7440dd4
Khajiit333 <marcin.marzynski@gmail.com>
2012-06-24 13:35:35 +0200

    WTF

--- WSDLResolver/src/pl/edu/agh/yamlconverter/YAMLConverter.java
+++ WSDLResolver/src/pl/edu/agh/yamlconverter/YAMLConverter.java
@@ -3,8 +3,14 @@ package pl.edu.agh.yamlconverter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
 import org.yaml.snakeyaml.Yaml;
 
 public class YAMLConverter {
@@ -20,7 +26,20 @@ public class YAMLConverter {
 		Object obj = yaml.load(io);
 		LinkedHashMap<String, Object> map = (LinkedHashMap<String, Object>) obj;
 		System.out.println(map);
-
+	}
+	
+	/**
+	 * Metoda wczytuje podany plik YAML i parsuje go do pliku WSDL
+	 * @param path
+	 * @throws ParserConfigurationException 
+	 */
+	public Document parse(String path) throws ParserConfigurationException {
+		//tworzenie nowego dokumentu
+		
+		
+						
+		
+		return null;
 	}
 
 }
